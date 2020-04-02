c2f8eed85380b3b8e302ade955bac42b6f1c7800
Александр <sacha3032104@gmail.com>
2020-04-02 10:18:36 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,44 +1,42 @@
 #!/bin/bash
 
 echo "[WA]: Installing webadmin..."
-sleep 1
 
 echo "[WA]: Installing Node.js..."
-sleep 1
 apt install nodejs
-echo "[WA]: Node.js installed."
-sleep 1
+echo "[WA]: Installed."
+sleep 2
 
 echo "[WA]: Installing 'npm'..."
-sleep 1
 apt install npm
-echo "[WA]: npm installed."
-sleep 1
+echo "[WA]: Installed."
+sleep 2
+
+echo "[WA]: Dependency installation..."
+npm install
+echo "[WA]: Installed."
+sleep 2
 
 echo "[WA]: Creating webadmin config file..."
-sleep 1
 node createConfig.js
-echo "[WA]: Config created."
-sleep 1
+echo "[WA]: Created."
+sleep 2
 
 echo "[WA]: Adding webadmin.service..."
-sleep 1
-mv webadmin.service /etc/systemd/system/webadmin.service
+cp webadmin.service /etc/systemd/system/webadmin.service
 systemctl daemon-reload
-echo "[WA]: webadmin.service added."
-sleep 1
+echo "[WA]: Added."
+sleep 2
 
 echo "[WA]: Adding webadmin to autorun..."
-sleep 1
 systemctl enable webadmin
 echo "[WA]: Added."
-sleep 1
+sleep 2
 
 echo "[WA]: Launch webadmin..."
-sleep 1
 systemctl start webadmin
 echo "[WA]: webadmin is running."
-sleep 1
+sleep 2
 
 echo "[WA]: webadmin installed."
 exit 0
